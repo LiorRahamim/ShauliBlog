160bc30eb96810905a249b82312e9d1f4fc36637
Shany Guy <gshany@gmail.com>
2019-07-17 22:26:48 +0300

    adding initial fan model

---
+++ ShauliBlog/Models/Fan.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ShauliBlog.Models
+{
+    public class Fan
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string sn { get; set; }
+        public string gender { get; set; }
+        public DateTime birthdat { get; set; }
+        public int clubSeniority { get; set; }
+    }
+}
