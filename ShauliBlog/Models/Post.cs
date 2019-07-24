575e30c0ecfc8247019c045d37a4253f4a6ab0ee
Idan3 <idanahal3@gmail.com>
2019-07-24 23:41:37 +0300

    Adding Demi posts to db.

--- ShauliBlog/Models/Post.cs
+++ ShauliBlog/Models/Post.cs
@@ -28,7 +28,7 @@ namespace ShauliBlog.Models
         [Required]
         public string Content { get; set; }
 
-        public Image Image { get; set; } // the image should be in the sql server
+        public String Image { get; set; } // the image should be in the sql server
         
         public String Video { get; set; } // the video should be a link to youtube
 
