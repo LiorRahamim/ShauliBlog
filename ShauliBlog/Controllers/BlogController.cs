2d7b7b08b6b4d52c842a64205dd2594de56030b5
Idan3 <idanahal3@gmail.com>
2019-07-27 17:07:17 +0300

    Fix connection problem (Wrong "GetView function name).

--- ShauliBlog/Controllers/BlogController.cs
+++ ShauliBlog/Controllers/BlogController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShauliBlog.DAL;
 
 namespace ShauliBlog.Controllers
 {
     public class BlogController : Controller
     {
+        private BlogContext db = new BlogContext();
+
         public ActionResult Blog()
         {
-            return View();
+            return View(db.Posts);
         }
     }
 }
