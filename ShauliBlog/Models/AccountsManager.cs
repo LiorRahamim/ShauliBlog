f48b1b115e09aa52e867dc8368ccad6baa5d67ec
Shany Guy <gshany@gmail.com>
2019-08-01 00:23:59 +0300

    adding initial accounts manager class and buttons

---
+++ ShauliBlog/Models/AccountsManager.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShauliBlog.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "identity number")]
+        public string IDNumber { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "The confirmation password doesn't match the password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
