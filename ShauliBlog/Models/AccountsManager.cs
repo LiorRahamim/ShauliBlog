a1bc67d9756f12f10c77af914db2fdc7abc79a79
Shany Guy <gshany@gmail.com>
2019-08-02 11:33:57 +0300

    redefining accounts manager model

--- ShauliBlog/Models/AccountsManager.cs
+++ ShauliBlog/Models/AccountsManager.cs
@@ -5,11 +5,12 @@ namespace ShauliBlog.Models
     public class RegisterModel
     {
         [Required]
-        [Display(Name = "identity number")]
-        public string IDNumber { get; set; }
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
 
         [Required]
-        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -18,5 +19,67 @@ namespace ShauliBlog.Models
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         public string ConfirmPassword { get; set; }
+
+        /**
+         * Extra layer that helps identifying the user
+         */
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Question")]
+        public string AutenticationQuestion { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Answer")]
+        public string AutenticationAnswer { get; set; }
+    }
+
+    public class ForgotPasswordModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+
+    public class ForgotPasswordAuthenticationModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Question")]
+        public string AutenticationQuestion { get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Answer")]
+        public string AutenticationAnswer { get; set; }
+    }
+
+    public class ResetPasswordModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "The confirmation password doesn't match the password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
     }
 }
